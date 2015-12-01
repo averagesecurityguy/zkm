f9ccb8fa1e54b76cc9575a093ce479078bfeddff
Stephen Haywood <stephen@averagesecurityguy.info>
2015-11-30 20:54:40 -0500

    Logging support and channel support for cleanup.

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Copyright 2015 LCI Technology Group, LLC
@@ -7,11 +7,16 @@
 # This script should be run by cron and will delete old messages until there
 # are no more than MAX_KEEP messages in the database. The MAX_KEEP value can
 # be set in the db.py script.
+import logging
 import db
+
+logging.basicConfig(level=logging.WARN)
 
 try:
     zdb = db.ZKMDatabase()
-    zdb.cleanup_messages()
+    channels = zdb.get_channels()
+    for channel in channels:
+        zdb.cleanup_messages(channel[0])
 
 except db.DatabaseException():
     pass
